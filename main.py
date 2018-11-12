a73e71e48b1611d3b807a778ca0356abdb79ea3a
文杰 邓 <AptX.DWJ@outlook.com>
2018-11-12 18:41:29 +0800

    Modify ouput accuracy

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-import my_cma
+from sklearn.datasets import make_classification
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import cross_val_score
+from my_bayes_opt import BayesianOptimization
+import numpy as np
 
-es = my_cma.CMAEvolutionStrategy(0.45019955799280803, 0.047292304159400896, 0.04785904960298248, 5 * [0], 0.8, {'seed':400000})
-self = es.optimize(my_cma.ff.rosen)
-print(self.result[1])
+x, y = make_classification(n_samples=1000, n_features=10, n_classes=2)
+rf = RandomForestClassifier()
+print(np.mean(cross_val_score(rf, x, y, cv=20, scoring='roc_auc')))
+
+def rf_cv(n_estimators, min_samples_split, max_features, max_depth):
+    val = cross_val_score(
+        RandomForestClassifier(n_estimators=int(n_estimators),
+            min_samples_split=int(min_samples_split),
+            max_features=min(max_features, 0.999),  # float
+            max_depth=int(max_depth),
+            random_state=2
+        ),
+        x, y, 'roc_auc', cv=5
+    ).mean()
+    return val
+
+rf_bo = BayesianOptimization(
+        rf_cv,
+        {'n_estimators': (10, 250),
+        'min_samples_split': (2, 25),
+        'max_features': (0.1, 0.999),
+        'max_depth': (5, 15)}
+    )
+
+rf_bo.maximize()
